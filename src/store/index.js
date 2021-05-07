e6f3eb53a7efdf3a10a01d0d1466bab516a807d5
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-07 23:58:00 +0900

    actions mutations state定義

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lists: []
   },
   mutations: {
+    //第一引数でstate, 第二引数ではコミット時に受け取るpayloadを指定できる
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, card: [] })
+    }
   },
   actions: {
+    //contextはstoreインスタンスのメソッドやプロパティ呼び出すオブジェクトを受け取ることができる引数
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    }
   },
   // modules: {
   // }
   getters: {
-    
+
   }
 })
