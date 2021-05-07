c167a1bad59498a2a249c4837baac034e484bc05
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-07 23:13:50 +0900

    データ属性title作成

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,25 @@
+<template>
+<!-- @はv-onの省略系 -->
+<!-- .preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぐ -->
+  <form action="addlist" @submit.prevent="addList">
+    <input v-model="title"
+      type="text"
+      class="text-input"
+      placeholder="Add_new_list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+<script>
+  export default {
+    data: function() {
+      return {
+        title: "",
+      }
+    },
+
+
+  }
+</script>
