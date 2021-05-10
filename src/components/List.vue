a8af92eacd16c32b30e2faf2d85543ad5c18e0d5
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-10 22:37:09 +0900

    テンプレート箇所作成

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" @click="removeList">✖️</div>
+    </div>
+  </div>
 </template>
 <script>
 </script>
