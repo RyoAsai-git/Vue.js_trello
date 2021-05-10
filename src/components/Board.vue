b8842c5550d33c630eda3e3bf2c7aa736d05d0c2
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-10 23:25:22 +0900

    リスト作成機能実装

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,16 +5,33 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <!-- : v-on:の省略系 -->
+        <list v-for="(item, index) in lists"
+          :key = "item.id"
+          :title = "item.title"
+          :listIndex = "index"
+        />
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 <script>
   import ListAdd from './ListAdd.vue'
+  import List from './List'
+  import { mapState } from 'vuex'
   
   export default {
     components: {
-      ListAdd
+      ListAdd,
+      List,
+    },
+    computed: {
+      // ...オブジェクトスプレッド演算子 computedのローカルのオブジェクトと一緒に定義できる
+      ...mapState([
+        'lists'
+      ]),
     },
   }
 </script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -7,4 +7,23 @@
   </div>
 </template>
 <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      listIndex: {
+        type: Number,
+        required: true
+      }
+    },
+    methods: {
+      removeList: function() {
+        if (confirm('本当にこのリストを削除しますか?')) {
+          this.$store.dispatch('removelist', { listIndex: this.listIndex })
+        }
+      },
+    }
+  }
 </script>
