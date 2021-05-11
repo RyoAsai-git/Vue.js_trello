66c12e30626971a2ad8409c927f3e5a08f15f8a6
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-11 21:57:27 +0900

    処理の流れ コメントアウトにて記載

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,13 @@ import Vuex from 'vuex'
 // Vuexを使用するには、Vue.jsの他のプラグインと同様にVue.jsでインスタンスを作る前に use メソッドで登録
 Vue.use(Vuex)
 
+
+// localStorage に保存されたデータには保持期間の制限はなく、
+// sessionStorage に保存されたデータはセッションが終わると同時に（ブラウザが閉じられたときに）クリアされる
+// localStorageは保存するデータを「キーと値」のセットで扱います。
 const savedLists = localStorage.getItem('trello-lists')
 
-
   // Vuexでのデータの流れ
-  // Actions → Mutations → State → Vue Component
   // state には状態を
   // getters には state 内の状態を元に算出した値を返す関数
   // mutations には state のデータを更新する関数
@@ -18,6 +20,7 @@ const store = new Vuex.Store({
   // stateの参照はgettersで行うことができる
   state: {
     // JSON.parse(取得するデータ)でオブジェクトに変換
+    // localStorageに保存されたリストがあれば取得、なければデフォルトのリスト配列を設置します。
     lists: savedLists ? JSON.parse(savedLists): [
       {
         title: 'Backlog',
@@ -51,6 +54,7 @@ const store = new Vuex.Store({
   },
   actions: {
     //contextはstoreインスタンスのメソッドやプロパティ呼び出すオブジェクトを受け取ることができる引数
+    // payloadはオブジェクト型で受け取ると、複数のプロパティを受け取ることができるため、推奨されています。
     addlist(context, payload) {
       context.commit('addlist', payload)
     },
