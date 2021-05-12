7d06633f5fb38245f2ddbde3639d7ebf9c7f66a6
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-12 15:41:50 +0900

    form作成

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <form class="addcard" @submit.prevent="addCardToList">
+    <input v-model = "body"
+      type="text"
+      class="text-input"
+      placeholder="Add new card"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
 </template>
 <script>
 
